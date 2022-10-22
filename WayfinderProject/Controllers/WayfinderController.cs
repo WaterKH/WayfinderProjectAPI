3c375b588863d700c2c47552affdf6485ab9d439
waterkh <waterkh@outlook.com>
2022-10-23 09:37:08 +1300

    Added AND search instead of OR - May replace or make optional to reduce SQL Query lag? - Addresses #10

--- WayfinderProject/Controllers/WayfinderController.cs
+++ WayfinderProject/Controllers/WayfinderController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using WayfinderProjectAPI.Data;
 using WayfinderProjectAPI.Data.DTOs;
@@ -48,28 +45,72 @@ namespace WayfinderProjectAPI.Controllers
             {
                 var worldsList = worlds.Split("::").Select(x => x.Trim());
 
-                results = results.Where(x => x.Worlds.Any(y => worldsList.Contains(y.Name)));
+                var resultIds = new List<int>();
+                foreach (var result in results.Include(x => x.Worlds).Where(x => x.Worlds.Any(y => worldsList.Contains(y.Name))))
+                {
+                    if (result == null || result.Worlds == null) continue;
+
+                    if (worldsList.All(x => result.Worlds.Select(y => y.Name).Any(y => y == x)))
+                    {
+                        resultIds.Add(result.Id);
+                    }
+                }
+
+                results = results.Where(x => resultIds.Contains(x.Id));
             }
 
             if (characters != null)
             {
                 var charactersList = characters.Split("::").Select(x => x.Trim());
 
-                results = results.Where(x => x.Characters.Any(y => charactersList.Contains(y.Name)));
+                var resultIds = new List<int>();
+                foreach (var result in results.Include(x => x.Characters).Where(x => x.Characters.Any(y => charactersList.Contains(y.Name))))
+                {
+                    if (result == null || result.Characters == null) continue;
+
+                    if (charactersList.All(x => result.Characters.Select(y => y.Name).Any(y => y == x)))
+                    {
+                        resultIds.Add(result.Id);
+                    }
+                }
+
+                results = results.Where(x => resultIds.Contains(x.Id));
             }
 
             if (areas != null)
             {
                 var areasList = areas.Split("::").Select(x => x.Trim());
+                
+                var resultIds = new List<int>();
+                foreach (var result in results.Include(x => x.Areas).Where(x => x.Areas.Any(y => areasList.Contains(y.Name))))
+                {
+                    if (result == null || result.Areas == null) continue;
 
-                results = results.Where(x => x.Areas.Any(y => areasList.Contains(y.Name)));
+                    if (areasList.All(x => result.Areas.Select(y => y.Name).Any(y => y == x)))
+                    {
+                        resultIds.Add(result.Id);
+                    }
+                }
+
+                results = results.Where(x => resultIds.Contains(x.Id));
             }
 
             if (music != null)
             {
                 var musicList = music.Split("::").Select(x => x.Trim());
 
-                results = results.Where(x => x.Music.Any(y => musicList.Contains(y.Name)));
+                var resultIds = new List<int>();
+                foreach (var result in results.Include(x => x.Music).Where(x => x.Music.Any(y => musicList.Contains(y.Name))))
+                {
+                    if (result == null || result.Music == null) continue;
+
+                    if (musicList.All(x => result.Music.Select(y => y.Name).Any(y => y == x)))
+                    {
+                        resultIds.Add(result.Id);
+                    }
+                }
+
+                results = results.Where(x => resultIds.Contains(x.Id));
             }
 
             if (queryString != null)
@@ -120,7 +161,7 @@ namespace WayfinderProjectAPI.Controllers
                     GameName = gameName,
                     SceneName = sceneName,
                     Lines = gameName == "Kingdom Hearts Union χ" ?
-                        new List<ScriptLineDto> { new ScriptLineDto { Character = "None", Line = "This will be updated after Damo279's big project." } } : 
+                        new List<ScriptLineDto> { new ScriptLineDto { Character = "None", Line = "This will be updated after Damo279's big project." } } :
                         new List<ScriptLineDto> { new ScriptLineDto { Character = "None", Line = "None" } }
                 };
             }
