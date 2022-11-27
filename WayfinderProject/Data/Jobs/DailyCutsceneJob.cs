d4a579af2f240df870cd66ded6a0c33ac61d258d
waterkh <waterkh@outlook.com>
2022-11-27 15:06:58 +1300

    Update to Daily Cutscene - There was an issue with the daily cutscene where 1) the connection string wasn't on DigitalOcean and 2) we weren't checking for non-empty lines so there was potential for errors to appear when looking up that Id later. Overall just refactored and it should be sorted now.

--- WayfinderProject/Data/Jobs/DailyCutsceneJob.cs
+++ WayfinderProject/Data/Jobs/DailyCutsceneJob.cs
@@ -18,23 +18,14 @@ namespace WayfinderProject.Data.Jobs
 
         public Task Execute(IJobExecutionContext context)
         {
-            var connectionString = Environment.GetEnvironmentVariable("DailyCutsceneConnectionString");
-            var connection = new MySqlConnection(connectionString);
-            connection.Open();
-
-            string countQuery = "SELECT COUNT(*) FROM wayfinderprojectdb.MA_Scene";
-
-            MySqlCommand countCommand = new MySqlCommand(countQuery, connection);
-            int count = Convert.ToInt32(countCommand.ExecuteScalar());
-
             DateTime now = DateTime.Now;
             Random random = new Random((int)now.Ticks);
 
             string dateCode = now.ToString("yyyyMMdd");
-            int randomSceneId = random.Next(0, count);
 
-            // Insert a row to a table (probably yyyymmdd as key, and the random scene id we choose)
-
+            var connectionString = Environment.GetEnvironmentVariable("DailyCutsceneConnectionString");
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
 
             string selectQuery = $"SELECT * FROM wayfinderprojectdb.DailyCutscenes WHERE DateCode = {dateCode}";
 
@@ -55,75 +46,80 @@ namespace WayfinderProject.Data.Jobs
             }
             selectResult.Close();
 
+            // Only create a daily tweet if don't already have one
             if (dailyCutscene == null)
             {
+                // Get random daily scene
+                string sceneCountQuery = $@"SELECT COUNT(*) AS RowCount FROM wayfinderprojectdb.MA_Scene 
+                                    INNER JOIN wayfinderprojectdb.Games ON Games.Id = MA_Scene.GameId 
+                                    INNER JOIN wayfinderprojectdb.Script ON Script.SceneName = MA_Scene.Name
+                                    INNER JOIN wayfinderprojectdb.ScriptLine ON Script.Id = ScriptLine.ScriptId
+                                    WHERE ScriptLine.Line <> 'None' AND LENGTH(ScriptLine.Line) < 278";
+
+                MySqlCommand sceneCountCommand = new MySqlCommand(sceneCountQuery, connection);
+                int sceneCount = Convert.ToInt32(sceneCountCommand.ExecuteScalar());
+
+                string sceneQuery = $@"SELECT MA_Scene.Id AS SceneId, MA_Scene.Name AS Name, MA_Scene.Link AS Link, Games.Name AS GameName, ScriptLine.Line AS Line FROM wayfinderprojectdb.MA_Scene 
+                                    INNER JOIN wayfinderprojectdb.Games ON Games.Id = MA_Scene.GameId 
+                                    INNER JOIN wayfinderprojectdb.Script ON Script.SceneName = MA_Scene.Name
+                                    INNER JOIN wayfinderprojectdb.ScriptLine ON Script.Id = ScriptLine.ScriptId
+                                    WHERE ScriptLine.Line <> 'None' AND LENGTH(ScriptLine.Line) < 278";
+
+                MySqlCommand sceneCommand = new MySqlCommand(sceneQuery, connection);
+                MySqlDataReader sceneResult = sceneCommand.ExecuteReader();
+
+                int randomSceneDialogue = random.Next(0, sceneCount);
+                int randomSceneId = 0;
+                int counter = 0;
+                string tweetText = string.Empty;
+                string subTweetText = string.Empty;
+                while (sceneResult.Read())
+                {
+                    if (counter != randomSceneDialogue)
+                    {
+                        counter += 1;
+                        continue;
+                    }
+
+                    string line = (string)sceneResult["Line"];
+                    string name = (string)sceneResult["Name"];
+                    string gameName = (string)sceneResult["GameName"];
+                    string link = (string)sceneResult["Link"];
+
+                    randomSceneId = (int)sceneResult["SceneId"];
+
+                    // Assemble tweet and subtweet
+                    tweetText = $"\"{line}\"";
+                    subTweetText = $"~\"{name}\" from {gameName}\r\n{link}";
+
+                    break;
+                }
+                sceneResult.Close();
+
                 string insertQuery = $"INSERT INTO wayfinderprojectdb.DailyCutscenes (DateCode, SceneId) VALUES ({dateCode}, {randomSceneId})";
 
                 MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection);
                 insertCommand.ExecuteReader();
-            }
 
-            // Send out daily tweet
-            string consumerKey = Environment.GetEnvironmentVariable("TwitterConsumerKey");
-            string consumerSecret = Environment.GetEnvironmentVariable("TwitterConsumerSecret");
-            string accessKey = Environment.GetEnvironmentVariable("TwitterAccessKey");
-            string accessSecret = Environment.GetEnvironmentVariable("TwitterAccessSecret");
+                // Send out daily tweet
+                string consumerKey = Environment.GetEnvironmentVariable("TwitterConsumerKey");
+                string consumerSecret = Environment.GetEnvironmentVariable("TwitterConsumerSecret");
+                string accessKey = Environment.GetEnvironmentVariable("TwitterAccessKey");
+                string accessSecret = Environment.GetEnvironmentVariable("TwitterAccessSecret");
 
-            TwitterClient userClient = new TwitterClient(consumerKey, consumerSecret, accessKey, accessSecret);
+                TwitterClient userClient = new TwitterClient(consumerKey, consumerSecret, accessKey, accessSecret);
 
-            // Get scene of the day
-            string sceneCountQuery = $@"SELECT COUNT(*) AS RowCount FROM wayfinderprojectdb.MA_Scene 
-                                    INNER JOIN wayfinderprojectdb.Games ON Games.Id = MA_Scene.GameId 
-                                    INNER JOIN wayfinderprojectdb.Script ON Script.SceneName = MA_Scene.Name
-                                    INNER JOIN wayfinderprojectdb.ScriptLine ON Script.Id = ScriptLine.ScriptId
-                                    WHERE MA_Scene.Id = {dailyCutscene.SceneId} AND ScriptLine.Line <> 'None' AND LENGTH(ScriptLine.Line) < 278";
 
-            MySqlCommand sceneCountCommand = new MySqlCommand(sceneCountQuery, connection);
-            int sceneCount = Convert.ToInt32(sceneCountCommand.ExecuteScalar());
+                // Tweet the results
+                var tweet = userClient.Tweets.PublishTweetAsync(tweetText).Result;
 
-            string sceneQuery = $@"SELECT MA_Scene.Name AS Name, MA_Scene.Link AS Link, Games.Name AS GameName, ScriptLine.Line AS Line FROM wayfinderprojectdb.MA_Scene 
-                                    INNER JOIN wayfinderprojectdb.Games ON Games.Id = MA_Scene.GameId 
-                                    INNER JOIN wayfinderprojectdb.Script ON Script.SceneName = MA_Scene.Name
-                                    INNER JOIN wayfinderprojectdb.ScriptLine ON Script.Id = ScriptLine.ScriptId
-                                    WHERE MA_Scene.Id = {dailyCutscene.SceneId} AND ScriptLine.Line <> 'None' AND LENGTH(ScriptLine.Line) < 278";
-
-            MySqlCommand sceneCommand = new MySqlCommand(sceneQuery, connection);
-            MySqlDataReader sceneResult = sceneCommand.ExecuteReader();
-
-            int randomSceneDialogue = random.Next(0, sceneCount);
-            int counter = 0;
-            string tweetText = string.Empty;
-            string subTweetText = string.Empty;
-            while (sceneResult.Read())
-            {
-                if (counter != randomSceneDialogue)
+                PublishTweetParameters parameters = new PublishTweetParameters
                 {
-                    counter += 1;
-                    continue;
-                }
-
-                string line = (string)sceneResult["Line"];
-                string name = (string)sceneResult["Name"];
-                string gameName = (string)sceneResult["GameName"];
-                string link = (string)sceneResult["Link"];
-
-                // Assemble tweet and subtweet
-                tweetText = $"\"{line}\"";
-                subTweetText = $"~\"{name}\" from {gameName}\r\n{link}";
-
-                break;
+                    InReplyToTweetId = tweet.Id,
+                    Text = subTweetText
+                };
+                var subTweet = userClient.Tweets.PublishTweetAsync(parameters).Result;
             }
-            sceneResult.Close();
-
-            // Tweet the results
-            var tweet = userClient.Tweets.PublishTweetAsync(tweetText).Result;
-
-            PublishTweetParameters parameters = new PublishTweetParameters
-            {
-                InReplyToTweetId = tweet.Id,
-                Text = subTweetText
-            };
-            var subTweet = userClient.Tweets.PublishTweetAsync(parameters).Result;
 
             connection.Close();
 
