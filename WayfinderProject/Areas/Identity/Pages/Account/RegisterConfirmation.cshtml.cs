b38334fd39b1d20398db00a637293dcda3861559
waterkh <waterkh@outlook.com>
2023-02-05 10:38:24 +1300

    Require email confirmation

--- WayfinderProject/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ WayfinderProject/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -2,15 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
 using WayfinderProject.Data.Models;
 
 namespace WayfinderProject.Areas.Identity.Pages.Account
@@ -61,7 +59,7 @@ namespace WayfinderProject.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
+            DisplayConfirmAccountLink = false;
             if (DisplayConfirmAccountLink)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
