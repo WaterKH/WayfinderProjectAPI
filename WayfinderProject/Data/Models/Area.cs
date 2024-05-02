dfd6c63febc455041066ffdc167adc8f4701d057
waterkh <waterkh@outlook.com>
2024-05-02 18:31:55 +1200

    Bug Fixes
    - Fix to Privated Music Tracks - Closes #192
    - Removed added dialogue - Closes #198
    - Fix to A Steadfast Friend - Closes #199
    - Split one scene into two scenes - Closes #201
    - Added The Prince back to Character selection - Closes #202
    - Fix to Maleficient's line in Maleficent's Designs - Closes #203
    - Removed Ansem the Wise from In a Flash of Darkness Scene - Closes #204

--- WayfinderProject/Data/Models/Area.cs
+++ WayfinderProject/Data/Models/Area.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
